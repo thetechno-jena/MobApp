a0067f53dca8ae063622f7c3519be886c7be1946
thetechno-jena <mail2jenakan@gmail.com>
2024-07-25 22:03:07 +0530

    Login Screen Updated

--- mobrepra/lib/Views/constants.dart
+++ mobrepra/lib/Views/constants.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+
 const String appName = 'Craftary';
 const String appSlogan = 'Make it Simple';
 const String bgImage = 'assets/images/Flower.JPG';
+const Color priColor = Color(0xff08c105);
--- mobrepra/lib/Views/login_screen.dart
+++ mobrepra/lib/Views/login_screen.dart
@@ -11,45 +11,123 @@ class LoginScreen extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Stack(
-            children: [
-              Image.asset(
-                bgImage,
-                height: height * 0.45,
-                width: width,
-                fit: BoxFit.cover,
-              ),
-              Container(
-                height: height * 0.45,
-                width: width,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        stops: [0.3, 0.8],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Colors.transparent, Colors.white])),
-                // color: Colors.orange.withOpacity(0.5),
-              ),
-            ],
-          ),
-          Text(
-            appName,
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w600,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Stack(
+              children: [
+                Image.asset(
+                  bgImage,
+                  height: height * 0.45,
+                  width: width,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  height: height * 0.45,
+                  width: width,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          stops: [0.3, 0.8],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Colors.transparent, Colors.white])),
+                  // color: Colors.orange.withOpacity(0.5),
+                ),
+                Positioned(
+                  bottom: 10,
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: Text(
+                      appName,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          Text(
-            appSlogan,
-            style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                color: Colors.grey.shade500),
-          )
-        ],
+            Center(
+              child: Text(
+                appSlogan,
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey.shade500),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Text(
+                  "  Login",
+                  style: TextStyle(fontSize: 18),
+                ),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      priColor.withOpacity(0.2),
+                      priColor.withOpacity(0.1)
+                    ]),
+                    border:
+                        Border(left: BorderSide(color: priColor, width: 6))),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: priColor)),
+                    prefixIcon: Icon(Icons.email, color: priColor),
+                    labelText: "Enter Your Email...",
+                    labelStyle: TextStyle(color: priColor)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: priColor)),
+                    prefixIcon: Icon(Icons.password, color: priColor),
+                    labelText: "Enter Your Password...",
+                    labelStyle: TextStyle(color: priColor)),
+              ),
+            ),
+            Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Forget Password?",
+                      style: TextStyle(color: priColor),
+                    ))),
+            Center(
+              child: SizedBox(
+                height: height * 0.08,
+                width: width,
+                child: TextButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(priColor)),
+                    onPressed: () {},
+                    child: Text(
+                      "Login to Account",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    )),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
