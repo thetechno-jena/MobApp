0e4fd77f0dec4103d103fd6c0ecdd357cfb95aad
thetechno-jena <mail2jenakan@gmail.com>
2024-07-25 14:33:47 +0530

    Overlay Style Applied

--- mobrepra/lib/Views/login_screen.dart
+++ mobrepra/lib/Views/login_screen.dart
@@ -10,14 +10,30 @@ class LoginScreen extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Image.asset(
-            bgImage,
-            height: height * 0.4,
-            width: width,
-            fit: BoxFit.cover,
+          Stack(
+            children: [
+              Image.asset(
+                bgImage,
+                height: height * 0.45,
+                width: width,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                height: height * 0.45,
+                width: width,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        stops: [0.3, 0.8],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.transparent, Colors.white])),
+                // color: Colors.orange.withOpacity(0.5),
+              ),
+            ],
           ),
           Text(
             appName,
@@ -26,8 +42,13 @@ class LoginScreen extends StatelessWidget {
               fontWeight: FontWeight.w600,
             ),
           ),
-          Text(appSlogan,
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300))
+          Text(
+            appSlogan,
+            style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.grey.shade500),
+          )
         ],
       ),
     );
